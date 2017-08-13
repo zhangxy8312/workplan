b1c7ab5f28b41d22b5798f36095fc03c5108c506
zhangxyw <50974251@qq.com>
2017-08-13 17:27:29 +0800

    Test Java

---
+++ ercpt/src/com/bitsoft/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.bitsoft.test;
+
+/**
+ * Created by zhangxy on 2017/8/13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello,World this is the first git project");
+    }
+}
